5206fd05a4f4085d6cd800272cb2bbb41ece6718
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-05-09 09:14:16 -0500

    double checking

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,8 +11,8 @@ INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY emp_no ASC;
-SELECT * FROM retiring_titles
-
+-- spend more time with where documentation
+--D.1.7 picture for read me
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 	rt.first_name,
 	rt.last_name,
@@ -20,9 +20,28 @@ SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 INTO Unique_titles
 FROM retirement_titles AS rt
 Order BY emp_no ASC, to_date DESC;
-
+--D.1.15 pic
 SELECT COUNT(ut.emp_no),ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+--D.1.21 pic
+
+--D2
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility
