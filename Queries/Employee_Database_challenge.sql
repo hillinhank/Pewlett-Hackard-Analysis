1498a1e12af4b509e1d94e7f084846f0130d9393
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-11-18 01:56:42 -0600

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,47 +1,55 @@
+-- using alias for clean code. need to get said columns from the employees and titles tables
 SELECT e.emp_no,
-    e.first_name,
+	e.first_name,
 	e.last_name,
-    ti.title,
-	ti.from_date,
-	ti.to_date
-INTO retirement_titles
+	t.title,
+	t.from_date,
+t.to_date
+-- use INTO to create a new table
+INTO ret_titles
 FROM employees as e
-INNER JOIN titles as ti ON (e.emp_no = ti.emp_no)
-INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+-- Join tables on primary keys and use where to filter for said age bracket
+INNER JOIN titles as t ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (de.to_date = '9999-01-01')
 ORDER BY emp_no ASC;
--- spend more time with where documentation
---D.1.7 picture for read me
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
-	rt.first_name,
-	rt.last_name,
-	rt.title
-INTO Unique_titles
-FROM retirement_titles AS rt
-Order BY emp_no ASC, to_date DESC;
---D.1.15 pic
-SELECT COUNT(ut.emp_no),ut.title
-INTO retiring_titles
-FROM unique_titles as ut
-GROUP BY title 
-ORDER BY COUNT(title) DESC;
---D.1.21 pic
 
---D2
+SELECT * FROM retiring_titles;
+
+-- Need to filter out employees who have left the company but are still in the system
+-- and place into a table
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO Unique_titles
+FROM ret_titles as rt
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- narrow it down to the employees who are in retirement age and get a head count by job title
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY title
+ORDER BY count(emp_no) DESC;
+
+-- need displayed columns from the employees, title, and dept_emp tables
+-- distinct on is needed to return the first instance of an employee being displayed in the system
 SELECT DISTINCT ON(e.emp_no)e.emp_no,
 e.first_name,
 e.last_name,
 e.birth_date,
 de.from_date,
 de.to_date,
-ti.title
+t.title
 INTO mentorship_eligibility
 FROM employees AS e
 INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
-INNER JOIN titles AS ti ON (e.emp_no = ti.emp_no)
+INNER JOIN titles AS t ON (e.emp_no = t.emp_no)
+-- filter on to_date and birthday columns
 WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+--reorgainze
 ORDER BY emp_no;
 
 SELECT * FROM mentorship_eligibility
